38e98fb34e42c6a27455ccc4d680740fcafacbb9
WorkingPranav <108082438+WorkingPranav@users.noreply.github.com>
2022-12-28 12:23:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ PICS = (environ.get('PICS', 'https://te.legra.ph/file/49dbb0c7a980949d9b09c.jpg'
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5608478737').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001414661907').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5608478737').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL', '0')
